fba35e6d7a28d3aad8112eab66cbd437673e2959
anasofia01 <sofiapacheco0423@gmail.com>
2024-03-31 17:04:54 -0500

    subo componente NavBar

---
+++ src/components/MainComponents/navBar/navBar.ts
@@ -0,0 +1,21 @@
+import styles from './navBar.css';
+
+class NavBar extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		const cssMyComponent = this.ownerDocument.createElement('style');
+		cssMyComponent.innerHTML = styles;
+		this.shadowRoot?.appendChild(cssMyComponent);
+	}
+}
+
+customElements.define('nav-bar', NavBar);
+export default NavBar;
