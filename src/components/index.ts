183052c4b66d874d4ad1050cbfb0f2ea81f39bc7
anasofia01 <sofiapacheco0423@gmail.com>
2024-03-31 17:07:13 -0500

    exporto los componentes NavBar y ButtonNav en index padre

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export { default as CardsContainer } from './MainComponents/cardsContainer/cards
 export { default as SingleCardPost } from './MainComponents/singleCardPost/singleCardPost';
 export { default as ContainerFriends } from './MainComponents/containerFriends/containerFriends';
 export { default as UserFriends } from './MainComponents/userFriend/userFriend';
+export { default as NavBar } from './MainComponents/navBar/navBar';
+export { default as ButtonNav } from './MainComponents/buttonNav/buttonNav';
