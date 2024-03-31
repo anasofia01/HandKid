36b97a970765f68184839c0d7f44efcd70855bd5
anasofia01 <sofiapacheco0423@gmail.com>
2024-03-31 16:13:44 -0500

    exporto mis componentes UserFriends y ContainerFriends

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as CardsContainer } from './MainComponents/cardsContainer/cardsContainer';
 export { default as SingleCardPost } from './MainComponents/singleCardPost/singleCardPost';
+export { default as ContainerFriends } from './MainComponents/containerFriends/containerFriends';
+export { default as UserFriends } from './MainComponents/userFriend/userFriend';
