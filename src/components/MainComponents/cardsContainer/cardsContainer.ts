5eff70c293a04e1ca77bf23b4e2100ef0b428899
anasofia01 <sofiapacheco0423@gmail.com>
2024-04-01 14:39:55 -0500

    le mando la data al componente CardsContainer

--- src/components/MainComponents/cardsContainer/cardsContainer.ts
+++ src/components/MainComponents/cardsContainer/cardsContainer.ts
@@ -1,6 +1,9 @@
+import SingleCardPost, { Attribute } from '../singleCardPost/singleCardPost';
 import styles from './cardsContainer.css';
+import { dataPost } from '../../../data/data';
 
 class CardsContainer extends HTMLElement {
+	cards: SingleCardPost[] = [];
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
@@ -8,9 +11,32 @@ class CardsContainer extends HTMLElement {
 
 	connectedCallback() {
 		this.render();
+		this.getValueData();
+	}
+
+	getValueData() {
+		const dataInfo = dataPost.map((user: any) => {
+			const postCard = this.ownerDocument.createElement('single-card-post') as SingleCardPost;
+			postCard.setAttribute(Attribute.avatar, user.person.avatar);
+			postCard.setAttribute(Attribute.name, user.person.name);
+			postCard.setAttribute(Attribute.username, user.person.username);
+			postCard.setAttribute(Attribute.description, user.post.description);
+			postCard.setAttribute(Attribute.timestamp, user.post.timestamp);
+			postCard.setAttribute(Attribute.hashtags, user.post.hashtags);
+			postCard.setAttribute(Attribute.media, user.post.media);
+			this.cards.push(postCard);
+		});
 	}
 
 	render() {
+		if (this.shadowRoot) {
+			const containerCards = this.ownerDocument.createElement('section');
+			this.cards.forEach((component) => {
+				containerCards.appendChild(component);
+				this.shadowRoot?.appendChild(containerCards);
+			});
+		}
+
 		const cssMyComponent = this.ownerDocument.createElement('style');
 		cssMyComponent.innerHTML = styles;
 		this.shadowRoot?.appendChild(cssMyComponent);
