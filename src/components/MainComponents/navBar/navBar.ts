4db7d6db44b701bccae9d68d771c22b440f7f629
anasofia01 <sofiapacheco0423@gmail.com>
2024-04-01 14:45:29 -0500

    corrijo componente NavBar

--- src/components/MainComponents/navBar/navBar.ts
+++ src/components/MainComponents/navBar/navBar.ts
@@ -1,3 +1,5 @@
+import ButtonNav, { Attribute } from '../buttonNav/buttonNav';
+import SingleCardPost from '../singleCardPost/singleCardPost';
 import styles from './navBar.css';
 
 class NavBar extends HTMLElement {
@@ -11,6 +13,16 @@ class NavBar extends HTMLElement {
 	}
 
 	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+			<button-nav id = "user"><i class = "fa-solid fa-user></i>"></button-nav>
+			<button-nav id = "addPost"><i class = "fa-light fa-circle-plus"></i></button-nav>
+			<button-nav id = "home"><i class = "fa-solid fa-house"></i></button-nav>
+			<button-nav id = "chat"><i class = "fa-solid fa-comments"></i></button-nav>
+			<button-nav id = "settings"><i class = "fa-solid fa-gear"></i></button-nav>
+			`;
+		}
+
 		const cssMyComponent = this.ownerDocument.createElement('style');
 		cssMyComponent.innerHTML = styles;
 		this.shadowRoot?.appendChild(cssMyComponent);
