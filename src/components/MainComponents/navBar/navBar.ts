9402254d74ed2c969e9de3ece6ed775feddcbb1b
anasofia01 <sofiapacheco0423@gmail.com>
2024-04-05 18:32:10 -0500

    fix button nav

--- src/components/MainComponents/navBar/navBar.ts
+++ src/components/MainComponents/navBar/navBar.ts
@@ -14,11 +14,11 @@ class NavBar extends HTMLElement {
 	render() {
 		if (this.shadowRoot) {
 			this.shadowRoot.innerHTML = `
-			<button-nav iconGeneral = "https://static.vecteezy.com/system/resources/previews/008/442/086/non_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg"></button-nav>
-			<button-nav iconGeneral = "https://cdn.pixabay.com/photo/2018/11/13/21/44/instagram-3814061_1280.png"></button-nav>
-			<button-nav iconGeneral = "https://cdn-icons-png.flaticon.com/512/25/25694.png"></button-nav>
-			<button-nav iconGeneral = "https://static.vecteezy.com/system/resources/previews/005/337/802/non_2x/icon-symbol-chat-outline-illustration-free-vector.jpg"></button-nav>
-			<button-nav iconGeneral = "https://static-00.iconduck.com/assets.00/settings-icon-1964x2048-8nigtrtt.png"></button-nav>
+			<button-nav icongeneral = "https://static.vecteezy.com/system/resources/previews/008/442/086/non_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg"></button-nav>
+			<button-nav icongeneral = "https://cdn.pixabay.com/photo/2018/11/13/21/44/instagram-3814061_1280.png"></button-nav>
+			<button-nav icongeneral = "https://cdn-icons-png.flaticon.com/512/25/25694.png"></button-nav>
+			<button-nav icongeneral = "https://static.vecteezy.com/system/resources/previews/005/337/802/non_2x/icon-symbol-chat-outline-illustration-free-vector.jpg"></button-nav>
+			<button-nav icongeneral = "https://static-00.iconduck.com/assets.00/settings-icon-1964x2048-8nigtrtt.png"></button-nav>
 			`;
 		}
 
