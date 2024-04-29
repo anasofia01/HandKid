1c1ece7ddfd8310035b2fb45d28bdc0fabf8d412
anasofia01 <sofiapacheco0423@gmail.com>
2024-04-29 18:32:34 -0500

    se ajusta y corrige el index padre

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,4 @@
 export { default as SingleCardPost } from './SingleCardPost/singleCardPost';
 export { default as SingleButtonNav } from './SingleButtonNav/singleButtonNav';
 export { default as SingleCardFriend } from './SingleCardFriend/singleCardFriend';
-/*
-export { default as ButtonPostInteractions } from './MainComponents/buttonPostInteractions/buttonPostInteractions';
-*/
+export { default as SingleCardProfile } from './SingleCardProfile/singleCardProfile';
