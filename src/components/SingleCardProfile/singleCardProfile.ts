4f5fb6f9d6255bbb46bc7a219ac80a3c29a442a1
anasofia01 <sofiapacheco0423@gmail.com>
2024-04-29 18:27:25 -0500

    creamos el componente single card profile

---
+++ src/components/SingleCardProfile/singleCardProfile.ts
@@ -0,0 +1,66 @@
+import styles from './singleCardProfile.css';
+
+class SingleCardProfile extends HTMLElement {
+	banner?: string;
+	avatar?: string;
+	name?: string;
+	username?: string;
+	age?: number;
+	friends?: number;
+
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+		if (name === 'banner') {
+			this.banner = newValue;
+		} else if (name === 'avatar') {
+			this.avatar = newValue;
+		} else if (name === 'name') {
+			this.name = newValue;
+		} else if (name === 'username') {
+			this.username = newValue;
+		} else if (name === 'age') {
+			this.age = newValue ? Number(newValue) : undefined;
+		} else if (name === 'friends') {
+			this.friends = newValue ? Number(newValue) : undefined;
+		}
+
+		this.render();
+	}
+
+	static get observedAttributes() {
+		return ['banner', 'avatar', 'name', 'username', 'age', 'friends'];
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+        <style>${styles}</style>
+        <div class = "container-profile">
+          <div class = "head-container">
+            <img class = "banner" src = "${this.banner}">
+            <img class = "avatar" src = "${this.avatar}">
+          </div>
+          <div class = "info-user-container">
+            <span class = "span-name">${this.name}</span>
+            <span class = "span-user-name">${this.username}</span>
+          </div>
+          <div class = "footer-container">
+            <span class = "span-age">${this.age}</span>
+            <span class = "span-friends">${this.friends}</span>
+          </div>
+        </div>
+      `;
+		}
+	}
+}
+
+customElements.define('single-card-profile', SingleCardProfile);
+export default SingleCardProfile;
