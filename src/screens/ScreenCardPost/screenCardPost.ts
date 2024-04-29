dc0561ffb0cc135fde3215ae42a28677774d0e44
anasofia01 <sofiapacheco0423@gmail.com>
2024-04-29 18:37:24 -0500

    se ajusta los estilos de css

--- src/screens/ScreenCardPost/screenCardPost.ts
+++ src/screens/ScreenCardPost/screenCardPost.ts
@@ -1,47 +1,46 @@
-import SingleCardPost, { Attribute } from '../../components/SingleCardPost/singleCardPost';
+import SingleCardPost from '../../components/SingleCardPost/singleCardPost';
 import styles from './screenCardPost.css';
 import { dataPost } from '../../data/data';
 
 class ScreenCardPost extends HTMLElement {
 	cards: SingleCardPost[] = [];
+
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
-		this.getValueData();
 	}
 
 	connectedCallback() {
 		this.render();
+		this.loadPosts();
 	}
 
-	getValueData() {
-		dataPost.forEach((user: any) => {
-			const postCard = this.ownerDocument.createElement('single-card-post') as SingleCardPost;
-			postCard.setAttribute(Attribute.avatar, user.person.avatar);
-			postCard.setAttribute(Attribute.name, user.person.name);
-			postCard.setAttribute(Attribute.username, user.person.username);
-			postCard.setAttribute(Attribute.description, user.post.description);
-			postCard.setAttribute(Attribute.timestamp, user.post.timestamp);
-			postCard.setAttribute(Attribute.hashtags, JSON.stringify(user.post.hashtags));
-			postCard.setAttribute(Attribute.media, JSON.stringify(user.post.media));
-			postCard.setAttribute(Attribute.likes, user.post.likes);
-			postCard.setAttribute(Attribute.comments, user.post.comments);
-			this.cards.push(postCard);
-		});
+	loadPosts() {
+		const postContainer = this.shadowRoot?.querySelector('.posts-cards-container');
+		if (postContainer) {
+			dataPost.forEach((post: any) => {
+				const postCard = this.ownerDocument.createElement('single-card-post') as SingleCardPost;
+				postCard.avatar = post.person.avatar;
+				postCard.name = post.person.name;
+				postCard.username = post.person.username;
+				postCard.description = post.post.description;
+				postCard.timestamp = post.post.timestamp;
+				postCard.hashtags = post.post.hashtags.map((tag: string) => tag.trim());
+				postCard.media = post.post.media.map((media: string) => media.trim());
+				postCard.likes = post.post.likes;
+				postCard.comments = post.post.comments;
+				postContainer.appendChild(postCard);
+			});
+		}
 	}
 
 	render() {
 		if (this.shadowRoot) {
-			const containerCards = this.ownerDocument.createElement('section');
-			this.cards.forEach((component) => {
-				containerCards.appendChild(component);
-			});
-			this.shadowRoot?.appendChild(containerCards);
+			this.shadowRoot.innerHTML = `
+				<style>${styles}</style>
+				<div class = "posts-cards-container"></div>
+				`;
 		}
-
-		const cssMyComponent = this.ownerDocument.createElement('style');
-		cssMyComponent.innerHTML = styles;
-		this.shadowRoot?.appendChild(cssMyComponent);
 	}
 }
 
