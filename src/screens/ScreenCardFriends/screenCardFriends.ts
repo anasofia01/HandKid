b42973c2c9ef19e6571e89faee667cf0fa79d231
anasofia01 <sofiapacheco0423@gmail.com>
2024-04-29 18:36:38 -0500

    se ajusta los estilos del css y se ajusta el componente

--- src/screens/ScreenCardFriends/screenCardFriends.ts
+++ src/screens/ScreenCardFriends/screenCardFriends.ts
@@ -4,49 +4,43 @@ import { dataFriends } from '../../data/data';
 
 class ScreenCardFriends extends HTMLElement {
 	users: SingleCardFriend[] = [];
+
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
-		this.getValueData();
 	}
 	connectedCallback() {
 		this.render();
+		this.loadFriends();
 	}
 
-	getValueData() {
-		dataFriends.forEach((user: any) => {
-			const userComponent = this.ownerDocument.createElement('single-card-friend') as SingleCardFriend;
-			userComponent.setAttribute(Attribute.avatar, user.avatar);
-			userComponent.setAttribute(Attribute.name, user.name);
-			userComponent.setAttribute(Attribute.username, user.username);
-			this.users.push(userComponent);
-		});
+	loadFriends() {
+		const friendsContainer = this.shadowRoot?.querySelector('.friends-cards-container');
+		if (friendsContainer) {
+			friendsContainer.innerHTML = '';
+			dataFriends.forEach((friend: any) => {
+				const friendComponent = this.ownerDocument.createElement('single-card-friend') as SingleCardFriend;
+				friendComponent.avatar = friend.avatar;
+				friendComponent.name = friend.name;
+				friendComponent.username = friend.username;
+				friendComponent.banner = friend.banner;
+				friendComponent.age = friend.age;
+				friendComponent.friends = friend.friends;
+				friendsContainer.appendChild(friendComponent);
+			});
+		}
 	}
 
 	render() {
 		if (this.shadowRoot) {
-			const containerTitle = this.ownerDocument.createElement('div');
-			const titleFriends = this.ownerDocument.createElement('h3');
-			titleFriends.textContent = 'Friends';
-			const iconSearch = this.ownerDocument.createElement('i');
-			iconSearch.className = 'fa-solid fa-magnifying-glass';
-
-			containerTitle.appendChild(titleFriends);
-			containerTitle.appendChild(iconSearch);
-
-			const containerFriends = this.ownerDocument.createElement('section');
-			containerFriends.appendChild(containerTitle);
-			containerFriends.className = 'container-friends';
-			this.users.forEach((component) => {
-				containerFriends.appendChild(component);
-			});
-
-			this.shadowRoot?.appendChild(containerFriends);
+			this.shadowRoot.innerHTML = `
+			<style>${styles}</style>
+			<div class = "container-friends">
+				<div class = "friends-cards-container">
+				</div>
+			</div>
+			`;
 		}
-
-		const cssMyComponent = this.ownerDocument.createElement('style');
-		cssMyComponent.innerHTML = styles;
-		this.shadowRoot?.appendChild(cssMyComponent);
 	}
 }
 
