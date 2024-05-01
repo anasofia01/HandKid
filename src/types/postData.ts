6ad98d9a3cae889d0aa4a9612e37e859b9f19280
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-01 11:57:16 -0500

    se crea la interfaz de post

---
+++ src/types/postData.ts
@@ -0,0 +1,13 @@
+export interface PostData {
+	avatar?: string;
+	name?: string;
+	username?: string;
+	description?: string;
+	timeStamp?: string;
+	likes?: number;
+	comments?: number;
+	media?: Array<string>;
+	images?: string;
+	hashtags?: Array<string>;
+	tags?: string;
+}
