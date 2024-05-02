4dc82b7d1a35be8a81233bfb7a92ae57d92be3cc
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-02 18:59:08 -0500

    se exporta la screen de create form post

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,3 +1,4 @@
 export { default as ScreenCardPost } from './ScreenCardPost/screenCardPost';
 export { default as ScreenCardFriends } from './ScreenCardFriends/screenCardFriends';
 export { default as ScreenNavBar } from './ScreenNavBar/screenNavBar';
+export { default as ScreenFormPost } from './ScreenFormPost/screenFormPost';
