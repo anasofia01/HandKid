86a21a6f2b7ee9c5f078656356c4801c5814a548
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-02 18:49:36 -0500

    se organiza el componente de Create form post

--- src/components/CreateFormPost/createFormPost.ts
+++ src/components/CreateFormPost/createFormPost.ts
@@ -1,3 +1,5 @@
+import styles from './createFormPost.css';
+
 class CreateFormPost extends HTMLElement {
 	constructor() {
 		super();
@@ -6,18 +8,42 @@ class CreateFormPost extends HTMLElement {
 
 	connectedCallback() {
 		this.render();
+		const form = this.shadowRoot?.querySelector('#form-create-post') as HTMLFormElement;
+		if (form) {
+			form.addEventListener('submit', (event) => {
+				event.preventDefault();
+				const formData = new FormData(form);
+				this.dispatchEvent(
+					new CustomEvent('form-submitted', {
+						detail: formData,
+						bubbles: true,
+						composed: true,
+					})
+				);
+			});
+		}
 	}
 
 	render() {
 		if (this.shadowRoot) {
 			this.shadowRoot.innerHTML = `
+				<style>${styles}</style>
         <div class = "container-form">
-          <form>
-            <input type = "text" placeHolder = "Add Media" name = "img2"></input>
-            <input type = "text" placeHolder = "Add Media" name = "img1"></input>
-            <input type = "text" placeHolder = "Description" name = "description"></input>
-            <input type = "text" placeHolder = "Hashtags" name = "tags1"></input>
-            <input type = "text" placeHolder = "Hashtags" name = "tags2"></input>
+          <form id = "form-create-post">
+						<div class = "image-inputs">
+							<input type = "text" placeHolder = "Add Media" name = "img1" required></input>
+							<input type = "text" placeHolder = "Add Media" name = "img2" required></input>
+						</div>
+						<div>
+							<textarea name="description" rows="10" cols="50" placeholder="Description" required></textarea>
+						</div>
+						<div class = "tag-inputs">
+							<input type = "text" placeHolder = "Hashtags" name = "tags1" required></input>
+							<input type = "text" placeHolder = "Hashtags" name = "tags2" required></input>
+						</div>
+						<div>
+							<input type = "submit" id = "save-post" value = "save"></input>
+						</div>
           </form>
         </div>
       `;
