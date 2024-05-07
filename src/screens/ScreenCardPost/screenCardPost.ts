810e36c54da655516bb735d23ab09731a1859112
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-06 19:54:32 -0500

    se reemplaza la data de services por la función getPosts de la base de datos de Firebase

--- src/screens/ScreenCardPost/screenCardPost.ts
+++ src/screens/ScreenCardPost/screenCardPost.ts
@@ -1,6 +1,6 @@
 import SingleCardPost from '../../components/SingleCardPost/singleCardPost';
 import styles from './screenCardPost.css';
-import { dataPost } from '../../services/data';
+import { getPosts } from '../../utils/firebase';
 
 class ScreenCardPost extends HTMLElement {
 	cards: SingleCardPost[] = [];
@@ -15,20 +15,22 @@ class ScreenCardPost extends HTMLElement {
 		this.loadPosts();
 	}
 
-	loadPosts() {
+	async loadPosts() {
 		const postContainer = this.shadowRoot?.querySelector('.posts-cards-container');
+		const dataNewPost = await getPosts();
 		if (postContainer) {
-			dataPost.forEach((post: any) => {
+			dataNewPost.forEach((post: any) => {
+				console.log(post);
 				const postCard = this.ownerDocument.createElement('single-card-post') as SingleCardPost;
-				postCard.avatar = post.person.avatar;
-				postCard.name = post.person.name;
-				postCard.username = post.person.username;
-				postCard.description = post.post.description;
-				postCard.timestamp = post.post.timestamp;
-				postCard.hashtags = post.post.hashtags.map((tag: string) => tag.trim());
-				postCard.media = post.post.media.map((media: string) => media.trim());
-				postCard.likes = post.post.likes;
-				postCard.comments = post.post.comments;
+				postCard.avatar = 'https://i.pinimg.com/474x/75/23/31/752331da9e7d5256b512f92c6de24849.jpg';
+				postCard.name = 'El Guapito';
+				postCard.username = '@guapito.2304';
+				postCard.description = post.description;
+				postCard.timestamp = post.timestamp;
+				postCard.hashtags = post.hashtags.map((tag: string) => tag.trim());
+				postCard.media = post.media.map((media: string) => media.trim());
+				postCard.likes = post.likes;
+				postCard.comments = post.comments;
 				postContainer.appendChild(postCard);
 			});
 		}
