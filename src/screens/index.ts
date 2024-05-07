c7d101658f52a775c4d3c330df607db772b18fb7
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-07 17:36:30 -0500

    se exporta la screen dashboard en el index de la carpeta screens

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,3 +2,4 @@ export { default as ScreenCardPost } from './ScreenCardPost/screenCardPost';
 export { default as ScreenCardFriends } from './ScreenCardFriends/screenCardFriends';
 export { default as ScreenNavBar } from './ScreenNavBar/screenNavBar';
 export { default as ScreenFormPost } from './ScreenFormPost/screenFormPost';
+export { default as screenDashboard } from './ScreenDashboard/screenDashboard';
