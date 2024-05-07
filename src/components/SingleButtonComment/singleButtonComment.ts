82d96173f78a8da5a60f1baec43dae80bde94cdc
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-07 17:10:21 -0500

    se crea la estructura html en el render

--- src/components/SingleButtonComment/singleButtonComment.ts
+++ src/components/SingleButtonComment/singleButtonComment.ts
@@ -8,7 +8,21 @@ class SingleButtonComment extends HTMLElement {
 		this.render();
 	}
 
-	render() {}
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+        <div class = "container-comment">
+          <div class = "user-info">
+            <img src = "" class = "avatar" alt = "avatar-user">
+            <small class = "username">Username:</small>
+          </div>
+          <div class = "description-comment">
+            <p class = "comment"></p>
+          </div>
+        </div>
+      `;
+		}
+	}
 }
 
 customElements.define('single-button-comment', SingleButtonComment);
