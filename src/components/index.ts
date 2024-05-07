aecbc69c430782159f4a228b965342b5c9917311
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-06 20:04:21 -0500

    se exportaron los nuevos componentes en el index padre

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,6 @@ export { default as SingleButtonNav } from './SingleButtonNav/singleButtonNav';
 export { default as SingleCardFriend } from './SingleCardFriend/singleCardFriend';
 export { default as SingleCardProfile } from './SingleCardProfile/singleCardProfile';
 export { default as CreateFormPost } from './CreateFormPost/createFormPost';
+export { default as SingleCardLogin } from './SingleCardLogin/singleCardLogin';
+export { default as SingleCardRegister } from './SingleCardRegister/singleCardRegister';
+export { default as SingleCardPassword } from './SingleCardPassword/singleCardPassword';
