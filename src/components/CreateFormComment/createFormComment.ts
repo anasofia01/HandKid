27778071e261d4bb70fea33c91cb5539dad3a601
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-07 18:47:33 -0500

    se crea la class CreateFormComment y se usan los metodos de la clase y también se exporta

--- src/components/CreateFormComment/createFormComment.ts
+++ src/components/CreateFormComment/createFormComment.ts
@@ -1,1 +1,14 @@
-...
+class CreateFormComment extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+	render() {}
+}
+
+customElements.define('create-form-comment', CreateFormComment);
+export default CreateFormComment;
