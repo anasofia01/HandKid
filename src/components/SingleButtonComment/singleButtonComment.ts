6ad7c4a1746ce59e096c53fede9e947811968697
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-07 17:05:49 -0500

    se crea la clase SingleButtonComment para el componente single button comment

---
+++ src/components/SingleButtonComment/singleButtonComment.ts
@@ -0,0 +1,15 @@
+class SingleButtonComment extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {}
+}
+
+customElements.define('single-button-comment', SingleButtonComment);
+export default SingleButtonComment;
