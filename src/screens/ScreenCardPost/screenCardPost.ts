22780ea3f0d4e7fd878583a27cff00c66649f621
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-07 17:58:41 -0500

    se elimina el console.log porque ya comprobamos la info que esta trayendo post.id

--- src/screens/ScreenCardPost/screenCardPost.ts
+++ src/screens/ScreenCardPost/screenCardPost.ts
@@ -23,7 +23,6 @@ class ScreenCardPost extends HTMLElement {
 				console.log(post);
 				const postCard = this.ownerDocument.createElement('single-card-post') as SingleCardPost;
 				postCard.idPost = post.id;
-				console.log(post.id);
 				postCard.avatar = 'https://i.pinimg.com/474x/75/23/31/752331da9e7d5256b512f92c6de24849.jpg';
 				postCard.name = 'El Guapito';
 				postCard.username = '@guapito.2304';
