513e687cb6ebb892262d27d883fbf43c7ef06859
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-09 16:32:56 -0500

    se importa la funcionalidad updateCommentsById en el screen y recibe los parametros idPost y newComments

--- src/screens/ScreenDashboard/screenDashboard.ts
+++ src/screens/ScreenDashboard/screenDashboard.ts
@@ -5,7 +5,13 @@ import { ProfileData } from '../../types/profileData';
 import { PostData } from '../../types/postData';
 import { CommentsData } from '../../types/commentsData';
 import SingleCardProfile from '../../components/SingleCardProfile/singleCardProfile';
-import { createPost, addCommentToPost, getCommentsById, getCommentById } from '../../utils/firebase';
+import {
+	createPost,
+	addCommentToPost,
+	getCommentsById,
+	getCommentById,
+	updateCommentsById,
+} from '../../utils/firebase';
 import { SingleButtonComment, SingleCardPost } from '../../components/index';
 
 class ScreenDashboard extends HTMLElement {
@@ -174,6 +180,7 @@ class ScreenDashboard extends HTMLElement {
 		const commentsPost = await getCommentById(idPost);
 		if (commentsPost !== null && commentsPost !== undefined) {
 			const newComments = commentsPost + 1;
+			await updateCommentsById(idPost, newComments);
 		}
 		await this.updateCommentsList(idPost);
 	}
