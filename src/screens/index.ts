294052385b29052cbafc3fe5449051d546a1c270
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-08 19:18:00 -0500

    se corrige error de la clase screen dashboard para que siga la misma nomenclatura

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,5 +2,5 @@ export { default as ScreenCardPost } from './ScreenCardPost/screenCardPost';
 export { default as ScreenCardFriends } from './ScreenCardFriends/screenCardFriends';
 export { default as ScreenNavBar } from './ScreenNavBar/screenNavBar';
 export { default as ScreenFormPost } from './ScreenFormPost/screenFormPost';
-export { default as screenDashboard } from './ScreenDashboard/screenDashboard';
+export { default as ScreenDashboard } from './ScreenDashboard/screenDashboard';
 export { default as ScreenCardLogin } from './ScreenCardLogin/screenCardLogin';
