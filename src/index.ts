47fb18e29a69ef80f76960483df2736577705b10
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-09 17:14:54 -0500

    se elimina la etiqueta screen dashboard del index abuelo

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,6 @@ class AppContainer extends HTMLElement {
 		if (this.shadowRoot) {
 			this.shadowRoot.innerHTML = `
         <style>${styles}</style>
-				<screen-dashboard></screen-dashboard>
       `;
 		}
 	}
