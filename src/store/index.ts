2571343e1980bca1811c5c01528cdd91c0a9e048
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-14 17:19:35 -0500

    se crea la const emptyState y estipula el estado inicial de la screen main

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,8 @@
 import { Observer } from '../types/store';
 import { Screens } from '../types/navigation';
 import { reducer } from './reducer';
+
+const emptyState = {
+	screen: Screens.MAIN,
+	user: {},
+};
