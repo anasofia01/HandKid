fcdf516dab67ed0abc8a07bfc391308ca96a761f
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-14 18:49:31 -0500

    se crea y se exporta el interface UserData con los atributos del usuario

---
+++ src/types/userData.ts
@@ -0,0 +1,12 @@
+export interface UserData {
+	fullname?: string;
+	username?: string;
+	birthdate?: string;
+	email?: string;
+	password?: string;
+	avatar?: string;
+	banner?: string;
+	friends?: number;
+	age?: number;
+	policy?: boolean;
+}
