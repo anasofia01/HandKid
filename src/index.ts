f6a198fa5bd711d20d596696e857130e17e64f2c
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-14 17:30:14 -0500

    se eliminan las funcionalidades que recibian los eventos personalizados

--- src/index.ts
+++ src/index.ts
@@ -11,51 +11,6 @@ class AppContainer extends HTMLElement {
 		this.render();
 	}
 
-	renderPageInit() {
-		const container = this.shadowRoot?.querySelector('.app-container');
-		if (container) {
-			container.innerHTML = '';
-			const screenPage = document.createElement('screen-page-init');
-			container.appendChild(screenPage);
-		}
-	}
-
-	renderLogin() {
-		const container = this.shadowRoot?.querySelector('.app-container');
-		if (container) {
-			container.innerHTML = '';
-			const screenLogin = document.createElement('screen-card-login');
-			container.appendChild(screenLogin);
-		}
-	}
-
-	renderRegister() {
-		const container = this.shadowRoot?.querySelector('.app-container');
-		if (container) {
-			container.innerHTML = '';
-			const screenRegister = document.createElement('screen-card-register');
-			container.appendChild(screenRegister);
-		}
-	}
-
-	renderPassword() {
-		const container = this.shadowRoot?.querySelector('.app-container');
-		if (container) {
-			container.innerHTML = '';
-			const screenPassword = document.createElement('screen-card-password');
-			container.appendChild(screenPassword);
-		}
-	}
-
-	renderDashboard() {
-		const container = this.shadowRoot?.querySelector('.app-container');
-		if (container) {
-			container.innerHTML = '';
-			const screenDashboard = document.createElement('screen-dashboard');
-			container.appendChild(screenDashboard);
-		}
-	}
-
 	render() {
 		if (this.shadowRoot) {
 			this.shadowRoot.innerHTML = `
