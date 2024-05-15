3cef0c07bf3baf9cb41cdbc742d79bff524d5ff1
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-15 18:52:52 -0500

    se crea el caso de SETUSER para recibir la info que le enviamos en actions, para que nos retorne el estado en el que esta la app

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -3,6 +3,9 @@ export const reducer = (action: any, preventState: any) => {
 		case 'NAVIGATE':
 			preventState.screen = action.payload;
 			break;
+		case 'SETUSER':
+			preventState.user = action.payload;
+			break;
 	}
 	return preventState;
 };
