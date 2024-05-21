ce7f7b71c954dc06060bf17ab2b2c9869f9c11e5
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-21 16:23:08 -0500

    se coloca en el render async y la info para que cree el post

--- src/screens/ScreenCardPost/screenCardPost.ts
+++ src/screens/ScreenCardPost/screenCardPost.ts
@@ -23,12 +23,31 @@ class ScreenCardPost extends HTMLElement {
 		}
 	}
 
-	render() {
+	async render() {
 		if (this.shadowRoot) {
 			this.shadowRoot.innerHTML = `
 				<style>${styles}</style>
 				<div class = "posts-cards-container"></div>
 				`;
+		}
+		const postContainer = this.shadowRoot?.querySelector('.posts-cards-container');
+		const dataNewPost = await getPosts();
+		if (postContainer) {
+			for (const post of dataNewPost) {
+				const postCard = this.ownerDocument.createElement('single-card-post') as SingleCardPost;
+				const userInfo = await getUserById(post.user || '');
+				postCard.idPost = post.id;
+				postCard.avatar = userInfo.avatar;
+				postCard.name = userInfo.fullname;
+				postCard.username = userInfo.username;
+				postCard.description = post.description;
+				postCard.timestamp = post.timestamp;
+				postCard.hashtags = post.hashtags?.map((tag: string) => tag.trim());
+				postCard.media = post.media?.map((media: string) => media.trim());
+				postCard.likes = post.likes;
+				postCard.comments = post.comments;
+				postContainer.appendChild(postCard);
+			}
 		}
 	}
 }
