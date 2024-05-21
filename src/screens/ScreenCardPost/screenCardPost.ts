6315f9a18a7869c7e405504d2707809eb24032d5
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-21 16:16:40 -0500

    se importa el appState

--- src/screens/ScreenCardPost/screenCardPost.ts
+++ src/screens/ScreenCardPost/screenCardPost.ts
@@ -2,7 +2,7 @@ import SingleCardPost from '../../components/SingleCardPost/singleCardPost';
 import styles from './screenCardPost.css';
 import { getPosts, getUserById, getUserLogin } from '../../utils/firebase';
 import { dataPost } from '../../services/data';
-import { addObserver } from '../../store';
+import { addObserver, appState } from '../../store';
 
 class ScreenCardPost extends HTMLElement {
 	cards: SingleCardPost[] = [];
