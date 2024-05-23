f7ee94b034fda94d20ec135a870488127c2ee705
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-22 19:54:50 -0500

    se crea la estructura html y se llaman los estilos de css

--- src/components/SingleCardSettings/singleCardSettings.ts
+++ src/components/SingleCardSettings/singleCardSettings.ts
@@ -10,7 +10,13 @@ class SingleCardSettings extends HTMLElement {
 		this.render();
 	}
 
-	render() {}
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+      <style>${styles}</style>
+      `;
+		}
+	}
 }
 
 customElements.define('single-card-settings', SingleCardSettings);
