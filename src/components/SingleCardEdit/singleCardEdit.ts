bd4d1de6aa12810ffcb5b186593a0c73c6c6bff4
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-23 18:26:51 -0500

    se crea la clase y se emplean los métodos

--- src/components/SingleCardEdit/singleCardEdit.ts
+++ src/components/SingleCardEdit/singleCardEdit.ts
@@ -1,1 +1,14 @@
 import styles from './singleCardEdit.css';
+
+class SingleCardEdit extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {}
+}
