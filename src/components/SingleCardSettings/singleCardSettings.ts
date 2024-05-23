08ec36047526e8657df7c04ca83140127501ad90
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-22 19:53:03 -0500

    se crea la clase y se emplean metodos

--- src/components/SingleCardSettings/singleCardSettings.ts
+++ src/components/SingleCardSettings/singleCardSettings.ts
@@ -1,1 +1,14 @@
 import styles from './singleCardSettings.css';
+
+class SingleCardSettings extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {}
+}
