7ff7ea7a96432b7603731d8c3f858c84c5c88bbf
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-22 19:56:27 -0500

    se crea la clase de la screen, emplean metodos, se define y exporta la clase

---
+++ src/screens/ScreenCardSettings/screenCardSettings.ts
@@ -0,0 +1,23 @@
+import styles from './screenCardSettings.css';
+
+class ScreenCardSettings extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+      <style>${styles}</style>
+      `;
+		}
+	}
+}
+
+customElements.define('screen-card-settings', ScreenCardSettings);
+export default ScreenCardSettings;
