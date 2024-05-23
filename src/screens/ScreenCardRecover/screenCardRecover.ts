7ea42f6afbf94a40bc6d10845c24a227815faddc
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-22 19:07:09 -0500

    se crea la clase y se emplean los metodos

--- src/screens/ScreenCardRecover/screenCardRecover.ts
+++ src/screens/ScreenCardRecover/screenCardRecover.ts
@@ -1,2 +1,15 @@
 import styles from './screenCardRecover.css';
 import '../../components/index';
+
+class ScreenCardRecover extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {}
+}
