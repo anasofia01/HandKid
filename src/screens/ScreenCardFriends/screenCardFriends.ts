da0ff88ab9c0cfa40b854ff7a731fa2e2e204e60
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-23 18:11:45 -0500

    se hace la condición de si los amigos en el appstate esta vacio que los guarde, sino que renderice

--- src/screens/ScreenCardFriends/screenCardFriends.ts
+++ src/screens/ScreenCardFriends/screenCardFriends.ts
@@ -16,6 +16,12 @@ class ScreenCardFriends extends HTMLElement {
 	async connectedCallback() {
 		this.render();
 		this.loadFriends();
+		if (appState.friends.length === 0) {
+			const action = await getFriendsAction();
+			dispatch(action);
+		} else {
+			this.render();
+		}
 	}
 
 	async loadFriends() {
