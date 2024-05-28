79ac3149e2900fd7229e4b430b5f0195f6ebacd4
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-28 18:56:08 -0500

    se crea la clase y se emplean los metodos

--- src/components/EditFormProfile/editFormProfile.ts
+++ src/components/EditFormProfile/editFormProfile.ts
@@ -1,3 +1,16 @@
 import styles from './editFormProfile.css';
 import { UserData } from '../../types/userData';
 import { getUserLogin, getUserById } from '../../utils/firebase';
+
+class EditFormProfile extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {}
+}
