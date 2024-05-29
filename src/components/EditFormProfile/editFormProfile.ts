89e3f19f035861e4e808712b488acdcb07057a53
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-29 17:26:31 -0500

    se importa onUserLogin

--- src/components/EditFormProfile/editFormProfile.ts
+++ src/components/EditFormProfile/editFormProfile.ts
@@ -1,6 +1,6 @@
 import styles from './editFormProfile.css';
 import { UserData } from '../../types/userData';
-import { getUserLogin, getUserById } from '../../utils/firebase';
+import { onUserLogin, getUserById } from '../../utils/firebase';
 
 class EditFormProfile extends HTMLElement {
 	constructor() {
