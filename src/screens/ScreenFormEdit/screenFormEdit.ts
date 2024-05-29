bc0a0865902f550cd5f60d0dbce044c6334625a8
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-29 17:57:46 -0500

    se crea la clase y se emplean los metodos

--- src/screens/ScreenFormEdit/screenFormEdit.ts
+++ src/screens/ScreenFormEdit/screenFormEdit.ts
@@ -1,2 +1,15 @@
 import styles from './screenFormEdit.css';
 import '../../components/index';
+
+class ScreenFormEdit extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {}
+}
