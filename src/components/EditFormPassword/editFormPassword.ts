a95093920191955b820249005e5736a0ec5078e1
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-29 18:53:47 -0500

    se crea la clase y se emplean los metodos

--- src/components/EditFormPassword/editFormPassword.ts
+++ src/components/EditFormPassword/editFormPassword.ts
@@ -1,1 +1,14 @@
 import styles from './editFormPassword.css';
+
+class EditFormPassword extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {}
+}
