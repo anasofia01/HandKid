3e61290602e024ea996ca5432dd853361a419d16
anasofia01 <sofiapacheco0423@gmail.com>
2024-05-30 16:13:09 -0500

    se crea la clase y se emplean los metodos

--- src/screens/ScreenEditPassword/screenEditPassword.ts
+++ src/screens/ScreenEditPassword/screenEditPassword.ts
@@ -1,2 +1,15 @@
 import styles from './screenEditPassword.css';
 import '../../components/index';
+
+class ScreenEditPassword extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {}
+}
